82d0836fd2b0d93acde0c90d527014f8a9f41651
shahadathhs <shahadathhossensajib732@gmail.com>
2024-08-15 12:10:49 +0600

    first functional test completed

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,33 @@
 'use strict';
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const express = require('express');
+const router = express.Router();
+const ConvertHandler = require('../controllers/convertHandler');
+const convertHandler = new ConvertHandler();
 
-module.exports = function (app) {
-  
-  let convertHandler = new ConvertHandler();
+router.get('/convert', (req, res) => {
+  const { input } = req.query;
 
+  const num = convertHandler.getNum(input);
+  const unit = convertHandler.getUnit(input);
+
+  if (num === "invalid number" || unit === "invalid unit") {
+    return res.json({ error: 'Invalid Input' });
+  }
+
+  const returnUnit = convertHandler.getReturnUnit(unit);
+  const convertedNum = convertHandler.convert(num, unit);
+  const string = convertHandler.getString(num, unit, returnUnit);
+
+  res.json({
+    initNum: num,
+    initUnit: unit,
+    returnNum: convertedNum,
+    returnUnit: returnUnit,
+    string: string
+  });
+});
+
+module.exports = function(app) {
+  app.use('/api', router);
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,36 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
-let assert = chai.assert;
-const server = require('../server');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
+const assert = chai.assert;
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite("Functional Tests", function () {
+  this.timeout(5000);
 
+  suite("Integration tests with chai-http", function () {
+    test("Convert a valid input such as 10L", function (done) {
+      chai
+        .request(server)
+        .get("/api/convert")
+        .query({ input: "10L" })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.property(res.body, "initNum");
+          assert.property(res.body, "initUnit");
+          assert.property(res.body, "returnNum");
+          assert.property(res.body, "returnUnit");
+          assert.property(res.body, "string");
+          assert.equal(res.body.initNum, 10);
+          assert.equal(res.body.initUnit, "L");
+          assert.equal(res.body.returnNum, 2.6417217685798895);
+          assert.equal(res.body.returnUnit, "gal");
+          assert.equal(
+            res.body.string,
+            "10 liters converts to 2.6417217685798895 gallons"
+          );
+          done();
+        });
+    });
+  });
 });
